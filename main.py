e3270c71ebc73bc0b45fc783a4c5d5030d70a0cd
Pierre-Henri Dezanneau <ph.dezanneau@gmail.com>
2024-08-16 11:06:29 +0200

    Remove warning

--- main.py
+++ main.py
@@ -7,14 +7,24 @@ from mqtt.mqtt_monitor import MqttClientMonitor, TerminationStatus
 from mqtt.mqtt_pub import MqttPub
 from mqtt.mqtt_sub import MqttSub
 
-QOS = 2
-
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s [%(levelname)s] %(message)s')
+MQTT_QOS = 2
 
 
-def main(args):
+def main():
+    parser = ArgumentParser()
+    parser.add_argument("--src-mqtt-server", required=True)
+    parser.add_argument("--src-mqtt-port", type=int, default=1883)
+    parser.add_argument("--src-mqtt-username")
+    parser.add_argument("--src-mqtt-password")
+    parser.add_argument("--dst-mqtt-server", required=True)
+    parser.add_argument("--dst-mqtt-port", type=int, default=1883)
+    parser.add_argument("--dst-mqtt-username")
+    parser.add_argument("--dst-mqtt-password")
+    parser.add_argument("--topic", required=True)
+    args = parser.parse_args()
+
     monitor = MqttClientMonitor()
-    publisher = MqttPub(monitor, args.dst_mqtt_server, args.dst_mqtt_port, args.topic, QOS,
+    publisher = MqttPub(monitor, args.dst_mqtt_server, args.dst_mqtt_port, args.topic, MQTT_QOS,
                         mqtt_username=args.dst_mqtt_username,
                         mqtt_password=args.dst_mqtt_password)
 
@@ -24,7 +34,7 @@ def main(args):
             f"Message '{msg}' forwarded "
             f"from {args.src_mqtt_server} to {args.dst_mqtt_server} on topic {topic}")
 
-    subscriber = MqttSub(monitor, args.src_mqtt_server, args.src_mqtt_port, args.topic, QOS, on_message,
+    subscriber = MqttSub(monitor, args.src_mqtt_server, args.src_mqtt_port, args.topic, MQTT_QOS, on_message,
                          mqtt_username=args.src_mqtt_username,
                          mqtt_password=args.src_mqtt_password)
 
@@ -39,27 +49,15 @@ def main(args):
     signal.signal(signal.SIGINT, signal_handler)
 
     logging.info("Mqtt forwarder is now ready")
-    status = monitor.wait_for_termination()
-    monitor.close_all_clients(status)
-    return status
-
-
-if __name__ == "__main__":
-    parser = ArgumentParser()
-    parser.add_argument("--src-mqtt-server", required=True)
-    parser.add_argument("--src-mqtt-port", type=int, default=1883)
-    parser.add_argument("--src-mqtt-username")
-    parser.add_argument("--src-mqtt-password")
-    parser.add_argument("--dst-mqtt-server", required=True)
-    parser.add_argument("--dst-mqtt-port", type=int, default=1883)
-    parser.add_argument("--dst-mqtt-username")
-    parser.add_argument("--dst-mqtt-password")
-    parser.add_argument("--topic", required=True)
-    args = parser.parse_args()
-
-    termination_status = main(args)
+    termination_status = monitor.wait_for_termination()
+    monitor.close_all_clients(termination_status)
 
     if termination_status == TerminationStatus.NORMAL_TERMINATION:
         sys.exit(0)
     elif termination_status == TerminationStatus.ABNORMAL_TERMINATION:
         sys.exit(1)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s [%(levelname)s] %(message)s')
+    main()
